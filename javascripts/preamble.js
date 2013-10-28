4b8e79c66b139a1a6724ce81602a94fb8e984810
jeffschwartz <jefftschwartz@gmail.com>
2013-10-28 12:49:31 -0400

    Bumped version from 1.0.7 to 1.0.8.

--- javascripts/preamble.js
+++ javascripts/preamble.js
@@ -1,12 +1,12 @@
 /*global preambleConfig*/
-//Preamble 1.0.7
+//Preamble 1.0.8
 //(c) 2013 Jeffrey Schwartz
 //Preamble may be freely distributed under the MIT license.
 (function(window, undefined){
     'use strict';
 
     //Version
-    var version = 'v1.0.7';
+    var version = 'v1.0.8';
     //Targeted DOM elements.
     var elPreambleContainer = document.getElementById('preamble-container');
     var elHeader;
