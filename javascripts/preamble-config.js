5b71562c57f2cb6c9f207438e5c4771248738694
jeffschwartz <jefftschwartz@gmail.com>
2014-06-12 15:58:25 -0400

    Removing short circuiting support for v2 release. Renaming asyncTestDelay to testTimeOutInterval.

--- javascripts/preamble-config.js
+++ javascripts/preamble-config.js
@@ -5,13 +5,10 @@
 
 /*
 var preambleConfig = {
-    shortCircuit: Default value = false. Set to true to terminate upon first failure.
     windowGlobals: Default value = true. Set to false if you don't want to pollute the global name space and instead use the two global vars 'Preamble' and 'assert'. *IMPORTANT - USING IN-LINE CONFIGURATION TO OVERRIDE THE "windowGlobals" OPTION IS NOT SUPPORTED.
-    asyncTestDelay: Default value = 10. Set the value used to wait before calling whenAsyncDone's callback.
-    asyncBeforeAfterTestDelay: Default value = 10. Set the value used to wait before calling the test's callback (asyncBeforeEachTest) and when calling the next test's callback (asyncAfterEachTest, respectively).
+    testTimeOutInterval: Default value = 10. Set the value used to wait before calling whenAsyncDone's callback.
     name: Default value = 'Test'. Override this to display a meaningful name for your tests.
     hidePassedTests: Default value =  false. Set to true to hide padded tests.
-    filters: default: [] - Set 1 or more filters by adding hashes, e.g. {group: groupLabel, test: testLabel, assertion: assertionLabel}. You can also use the wildcard '*' character for test and/or assertions to specify that all tests and/or all assertions, respectively, should be included in the filter.
     uiTestContainerId: Default value = 'ui-test-container'. Override this to use a different id.
 };
 */
