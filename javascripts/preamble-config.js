f243ac74ad99a40a5b2d1046184e30e970ced23f
jeffschwartz <jefftschwartz@gmail.com>
2015-03-17 17:11:24 -0400

    Modified preamble-config.js to bring it up to v2.0.

--- javascripts/preamble-config.js
+++ javascripts/preamble-config.js
@@ -3,12 +3,11 @@
  *Don't forget to uncomment this file's script tag in index.html.
  */
 
-/*
-var preambleConfig = {
-    windowGlobals: Default value = true. Set to false if you don't want to pollute the global name space and instead use the two global vars 'Preamble' and 'assert'. *IMPORTANT - USING IN-LINE CONFIGURATION TO OVERRIDE THE "windowGlobals" OPTION IS NOT SUPPORTED.
-    testTimeOutInterval: Default value = 10 milliseconds. Set to some other value used to wait before a test times out.
-    name: Default value = 'Test'. Override this to display a meaningful name for your tests.
-    hidePassedTests: Default value =  false. Set to true to hide padded tests.
-    uiTestContainerId: Default value = 'ui-test-container'. Override this to use a different id.
-};
-*/
+// var preambleConfig = {
+//     windowGlobals: true,
+//     testTimeOutInterval: 10,
+//     name: 'Test',
+//     uiTestContainerId: 'ui-test-container',
+//     hidePassedTests: false,
+//     hideAssertions: true
+// };
