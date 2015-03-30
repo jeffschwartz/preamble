87a09417a22f77ec663cb93bd79787348592123b
Jeffrey Schwartz <Jeff@192.168.1.8>
2015-03-30 18:43:59 -0400

    Changed the order of the suites.

--- javascripts/sample-failures-test.js
+++ javascripts/sample-failures-test.js
@@ -7,6 +7,16 @@
  *some of the following tests are coded to intentionally fail.
  */
 
+describe('Running a test', function(){
+    it('and it passes it looks like this', function(){
+        var hw = 'Hello World!';
+        isTrue(hw === 'Hello World!');
+    });
+    it('and if it fails it looks like this', function(){
+        isTrue(false);
+    });
+});
+
 describe('A nested test fails', function(){
     it('a passing test looks like this', function(){
         equal(1, 1);
@@ -20,6 +30,43 @@ describe('A nested test fails', function(){
                 equal(1, 0);
             });
         });
+    });
+});
+
+/**
+ * This test will take 1000 miliseconds to run but the test will
+ * time out and fail because asyncTestDelay is set above to 500.
+ */
+describe('A long running test that fails to complete on time', function(){
+    var count = 0;
+    it('will time out and marked as having failed', function(done){
+        setTimeout(function(){
+            count = 100;
+            done(function(){
+                equal(count, 100);
+            });
+        }, 1000);
+    });
+});
+
+/**
+ * This is the same test as above but here the tests sets its
+ * time out interval to 1010 miliseconds to prevent it from
+ * timing out and failing.
+ * This is a very good way to fine tune individual test but...
+ * IMPORTANT: you can override asyncTestDelay in your in-line
+ * configuration or in your configuration file and it will
+ * apply to all tests.
+ */
+describe('A long running test can change how long its timeout interval is', function(){
+    var count = 0;
+    it('so it will not time out and fail.', 1010, function(done){
+        setTimeout(function(){
+            count = 100;
+            done(function(){
+                equal(count, 100);
+            });
+        }, 1000);
     });
 });
 
@@ -75,57 +122,10 @@ describe('A test can configure long running asynchronous after processes not to 
             done();
         }, 1000);
     });
-    it('by passing a time out interval', 1010, function(){
+    it('by passing a time out interval', 1050, function(){
         equal(this.count, 100);
     });
-    it('count should be reset to 100', 1010, function(){
+    it('count should be reset to 100', 1050, function(){
         equal(this.count, 100);
-    });
-});
-
-/**
- * This test will take 1000 miliseconds to run but the test will
- * time out and fail because asyncTestDelay is set above to 500.
- */
-describe('A long running test that fails to complete on time', function(){
-    var count = 0;
-    it('will time out and marked as having failed', function(done){
-        setTimeout(function(){
-            count = 100;
-            done(function(){
-                equal(count, 100);
-            });
-        }, 1000);
-    });
-});
-
-/**
- * This is the same test as above but here the tests sets its
- * time out interval to 1010 miliseconds to prevent it from
- * timing out and failing.
- * This is a very good way to fine tune individual test but...
- * IMPORTANT: you can override asyncTestDelay in your in-line
- * configuration or in your configuration file and it will
- * apply to all tests.
- */
-describe('A long running test can change how long its timeout interval is', function(){
-    var count = 0;
-    it('so it will not time out and fail.', 1010, function(done){
-        setTimeout(function(){
-            count = 100;
-            done(function(){
-                equal(count, 100);
-            });
-        }, 1000);
-    });
-});
-
-describe('Running a test', function(){
-    it('and it passes it looks like this', function(){
-        var hw = 'Hello World!';
-        isTrue(hw === 'Hello World!');
-    });
-    it('and if it fails it looks like this', function(){
-        isTrue(false);
     });
 });
