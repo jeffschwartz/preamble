19ac9465b3ddd31e2b46d14c15c00b4c410c1ef6
Jeff Schwartz <jefftschwartz@gmail.com>
2015-06-30 06:27:39 -0400

    Added dist and src folders and began modularizing core Preamble components.

---
+++ src/javascripts/core/pubsub.js
@@ -0,0 +1,97 @@
+(function(){
+    'use strict';
+
+    /**
+     * Events - publish/subscribe.
+     */
+    module.exports = (function(){
+        //subscribers is a hash of hashes:
+        //{'some topic': {'some token': callbackfunction, 'some token': callbackfunction, . etc. }, . etc }
+        var subscribers = {},
+            totalSubscribers = 0,
+            lastToken = 0;
+        //Generates a unique token.
+        function getToken(){
+            return lastToken += 1;
+        }
+        //Returns a function bound to a context.
+        function bindTo(fArg, context){
+            return fArg.bind(context);
+        }
+        //Returns a function which wraps subscribers callback in a setTimeout callback.
+        function makeAsync(topic, callback){
+            return function(data){
+                setTimeout(function(){
+                    callback(topic, data);
+                }, 1);
+            };
+        }
+        //Adds a subscriber for a topic with a callback
+        //and returns a token to allow unsubscribing.
+        function on(topic, handler, context){
+            var token = getToken(),
+                boundAsyncHandler;
+            boundAsyncHandler = context && makeAsync(topic,
+                bindTo(handler, context)) || makeAsync(topic,
+                handler);
+            //Add topic to subscribers if it doesn't already have it.
+            if(!subscribers.hasOwnProperty(topic)){
+                subscribers[topic] = {};
+            }
+            //Add subscriber to subscribers.
+            subscribers[topic][token] = boundAsyncHandler;
+            //Maintain a count of total subscribers.
+            totalSubscribers++;
+            //Return the token to the caller so it can unsubscribe.
+            return token;
+        }
+        //Removes a subscriber for a topic.
+        function off(topic, token){
+            if(subscribers.hasOwnProperty(topic)){
+                if(subscribers[topic].hasOwnProperty(token)){
+                    delete subscribers[topic][token];
+                    totalSubscribers--;
+                }
+            }
+        }
+        //Publishes an event for a topic with optional data.
+        function emit(topic, data){
+            var token;
+            if(subscribers.hasOwnProperty(topic)){
+                for(token in subscribers[topic]){
+                    if(subscribers[topic].hasOwnProperty(token)){
+                        if(arguments.length > 1){
+                            subscribers[topic][token](data);
+                        } else {
+                            subscribers[topic][token]();
+                        }
+                    }
+                }
+            }
+        }
+        //Returns the total subscribers count.
+        function getCountOfSubscribers(){
+            return totalSubscribers;
+        }
+        //Returns the subscriber count by topic.
+        function getCountOfSubscribersByTopic(topic){
+            var prop, count = 0;
+            if(subscribers.hasOwnProperty(topic)){
+                for(prop in subscribers[topic]){
+                    if(subscribers[topic].hasOwnProperty(prop)){
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+        //Returns the object that exposes the pubsub API.
+        return {
+            on: on,
+            off: off,
+            emit: emit,
+            getCountOfSubscribers: getCountOfSubscribers,
+            getCountOfSubscribersByTopic: getCountOfSubscribersByTopic
+        };
+    }());
+}());
